737980ebb7d06928bbfa8b235076457ed731c22d
Emiliano Enriquez <enriquez.emiliano15@gmail.com>
2024-08-10 17:59:43 -0600

    Terminando con actualizar y eliminar servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -44,18 +44,35 @@ class ServicioController {
     public static function actualizar( Router $router ) {
         session_start();
 
+        if(!is_numeric($_GET['id'])) header('Location: /servicios');
+
+        $servicio = Servicio::find($_GET['id']);
+        $alertas = [];
+
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        
+            $servicio->sincronizar($_POST);
+
+            $alertas = $servicio->validar();
+            if(empty($alertas)) {
+                $servicio->guardar();
+                header('Location: /servicios');
+            }
         }
 
         $router->render('servicios/actualizar', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'servicio' => $servicio,
+            'alertas' => $alertas
         ]);
     }
 
     public static function eliminar( Router $router ) {
-        $router->render('servicios/index', [
-            
-        ]);
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+
+            $servicio = Servicio::find($id);
+            $servicio->eliminar();
+            header('Location: /servicios');
+        }
     }
 }
--- views/servicios/actualizar.php
+++ views/servicios/actualizar.php
@@ -1,6 +1,19 @@
-<h1 class="nombre-pagina">Nuevo Servicio</h1>
+<h1 class="nombre-pagina">Actualizar Servicio</h1>
 <p class="descripcion-pagina">Modifica los valores del formulario para actualizar el servicio</p>
 
 <?php 
     include_once __DIR__ . "/../templates/barra.php";
+    include_once __DIR__ . "/../templates/alertas.php";
 ?>
+
+<form 
+    method="POST"
+    class="formulario"
+>
+    <?php include_once __DIR__ . "/formulario.php"; ?>
+
+    <input 
+        type="submit"
+        class="boton"
+        value="Actualizar Servicio">
+</form>
